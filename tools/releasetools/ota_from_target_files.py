51aef32236b63cdc4d9575c0a882500769e3e7e6
saikiran2001 <bjsaikiran@gmail.com>
2021-01-25 04:28:53 +0100

    fixup! Show our rom info while flashing
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -826,7 +826,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   build_id = target_info.GetBuildProp("ro.build.id")
   build_date = target_info.GetBuildProp("ro.build.date")
   security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
-  device = target_info.GetBuildProp("ro.product.device")
+  device = target_info.GetBuildProp("ro.build.product")
 
   script.Print("------------------------------------------");
   script.Print(" Android version: %s"%(android_version));
