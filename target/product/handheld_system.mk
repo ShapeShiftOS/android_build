yennutheriyale/platform_build

Build Google audio files from android12 [1/2]
Change-Id: Ib20341d418c9f827ab86e60d52724d96b8d7e31a
Signed-off-by: Jason Edson <jaysonedson@gmail.com>

target/product/handheld_system.mk
@@ -84,5 +84,5 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_VENDOR_PROPERTIES += \
     ro.carrier?=unknown \
-    ro.config.notification_sound?=OnTheHunt.ogg \
-    ro.config.alarm_alert?=Alarm_Classic.ogg
+    ro.config.notification_sound?=Popcorn.ogg \
+    ro.config.alarm_alert?=Bright_morning.ogg

ShapeShiftOS/android_build

target/product/handheld_system.mk
@@ -84,9 +84,5 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_VENDOR_PROPERTIES += \
     ro.carrier?=unknown \
-
-ifeq ($(SSOS_BUILD),)
-PRODUCT_VENDOR_PROPERTIES += \
-    ro.config.notification_sound?=OnTheHunt.ogg \
-    ro.config.alarm_alert?=Alarm_Classic.ogg
-endif
+    ro.config.notification_sound?=Eureka.ogg \
+    ro.config.alarm_alert?=Fresh_start.ogg